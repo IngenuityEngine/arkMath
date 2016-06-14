fc7ba3c8454c5317f2dab89fdac22922b6c930b0
Grant Miller <blented@gmail.com>
2016-06-13 21:03:41 -0700

    added ensure vector and tests

--- arkMath/helpers.py
+++ arkMath/helpers.py
@@ -3,3 +3,9 @@ import Vec
 
 def isVector(testVec):
 	return isinstance(testVec, Vec.Vec)
+
+def ensureVector(x, y=0, z=0, w=0):
+	if isVector(x):
+		return x
+	else:
+		return Vec.Vec(x, y, z, w)
--- test/test_helpers.py
+++ test/test_helpers.py
@@ -12,10 +12,32 @@ import arkMath
 class test(tryout.TestSuite):
 	title = 'test/test_vec.py'
 
-	def init(self):
+	def is_vector(self):
 		vec = arkMath.Vec(1,2,3,4)
 		self.assertEqual(arkMath.isVector(vec), True)
 		self.assertEqual(arkMath.isVector(12), False)
+
+	def ensure_vector(self):
+		vec = arkMath.Vec(1,2,3,4)
+		ensured = arkMath.ensureVector(vec)
+		self.assertEqual(ensured.x, vec.x)
+
+		ensured = arkMath.ensureVector(12)
+		self.assertEqual(ensured.x, 12)
+		self.assertEqual(ensured.y, 0)
+
+		ensured = arkMath.ensureVector(12, 5, 4, 9)
+		self.assertEqual(ensured.x, 12)
+		self.assertEqual(ensured.y, 5)
+		self.assertEqual(ensured.z, 4)
+		self.assertEqual(ensured.w, 9)
+
+		ensured = arkMath.ensureVector([15, 25, 7, 2])
+		self.assertEqual(ensured.x, 15)
+		self.assertEqual(ensured.y, 25)
+		self.assertEqual(ensured.z, 7)
+		self.assertEqual(ensured.w, 2)
+
 
 if __name__ == '__main__':
 	tryout.run(test)
